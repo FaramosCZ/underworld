614a6d7658cb02117ab6909515b2a5cde4361dd3
FaramosCZ <mschorm@centrum.cz>
2018-06-10 03:35:17 +0200

    Fix wrong title

--- menu/menu_leve__hraci.php
+++ menu/menu_leve__hraci.php
@@ -7,5 +7,5 @@
  <a href="http://underworld.clanweb.eu/hra/hraci.php?page=Hraci_02_co_te_ceka" title="Co Tě čeká">Co Tě čeká</a><hr>
  <a href="http://underworld.clanweb.eu/hra/hraci.php?page=Hraci_02_pravidla_hry" title="Pravida hry">Pravidla hry</a><hr>
  <a href="http://underworld.clanweb.eu/hra/hraci.php?page=Hraci_02_terminy_akci" title="Termíny akcí">Termíny akcí</a><hr>
- <a href="http://underworld.clanweb.eu/hra/hraci.php?page=Hraci_02_organizatori" title="Výber postava">Organizátoři</a><hr>
+ <a href="http://underworld.clanweb.eu/hra/hraci.php?page=Hraci_02_organizatori" title="Organizátoři">Organizátoři</a><hr>
  <a href="?page=logout" title="Odhlásit se"><br>Odhlásit se</a><hr>
