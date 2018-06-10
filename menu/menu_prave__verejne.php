15fe18895ad9b233f58489a674836515112d808d
FaramosCZ <mschorm@centrum.cz>
2018-06-10 03:34:28 +0200

    Fix link

--- menu/menu_prave__verejne.php
+++ menu/menu_prave__verejne.php
@@ -4,5 +4,5 @@ if( session_id() )
    echo '<a class="menu_a" href="?page=login">Přihlaš se</a>
          <a class="menu_a" href="?page=register">Zaregistruj se</a>';
   }
-else echo '<a class="menu_a" href="php_lib/logout.php">Odhlásit</a>';
+else echo '<a class="menu_a" href="?page=logout.php">Odhlásit</a>';
 ?>
