d6b18fe6b53f0f4f168bc930e16e74ae5c8621c5
Michal Schorm <mschorm@redhat.com>
2018-06-10 02:52:03 +0200

    Style both menus the same way - remove right menu vertical center

--- menu/menu_prave__verejne.php
+++ menu/menu_prave__verejne.php
@@ -1,24 +1,10 @@
+<a href="?"><img src=obrazky/vamp_red_mensi.png style="width:100%" /></a>
+
 <?php
 if( session_id() )
-{
-?>
-
-<div class="menu_vertical_spacer">
-<a class="menu_login" href="php_lib/login.php">Přihlaš se</a>
-<a class="menu_login" href="php_lib/register.php">Zaregistruj se</a>
-</div>
-
-<?php
-}
-else
-{
-?>
-
-<div class="menu_vertical_spacer">
-<a class="menu_login" href="php_lib/logout.php">Odhlásit</a>
-</div>
-
-
-<?php
-}
+  {
+   echo '<a class="menu_a" href="php_lib/login.php">Přihlaš se</a>
+         <a class="menu_a" href="php_lib/register.php">Zaregistruj se</a>';
+  }
+else echo '<a class="menu_a" href="php_lib/logout.php">Odhlásit</a>';
 ?>
