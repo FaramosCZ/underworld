db52eed992edff18c11f47c83660e3e9019b1b08
xjarik <40128057+xjarik@users.noreply.github.com>
2018-06-26 17:31:21 +0200

    verify registration and insert to database
    verify registration data, check user isn't in database yet.
    Insert new player to database table "player_new".

--- php_lib/objects/database.php
+++ php_lib/objects/database.php
@@ -34,6 +34,21 @@ public function test()
    echo "test databaze je ok!<br />";
   }
 
+// funkce na kontrolu textu před vkládáním do databáze
+public function text_check($input_text)
+  {
+   // test zda je vůbec zadána hodnota  
+   if (isset($input_text) && !empty($input_text) )
+   {
+    //odstranění mezer v textu
+    $verified_text = preg_replace('/\s\s+/', ' ', $input_text);
+    //zachovám alespoň podrtržítko - alternativně $verified_text=preg_replace('[^0-9a-z\-\_]', '', $verified_text);
+    $verified_text=preg_replace('[^0-9a-z\-]', '', $verified_text);
+
+    return $verified_text;  
+   }
+  }
+ 
 
 public function verify_player($login,$password)
   {
@@ -43,7 +58,7 @@ public function verify_player($login,$password)
       //die("Database connection failed: ". $this->conn->connect_error); 
      }
 
-     $res = $this->conn->query("SELECT `id`,`nick` FROM `players` WHERE `nick`='" . $login . "' OR `email`='" . $login . "' AND `password`='" . $password . "'");
+     $res = $this->conn->query("SELECT `id`,`nick`,`email` FROM `players` WHERE `nick`='" . $login . "' OR `email`='" . $login . "' AND `password`='" . $password . "'");
      
       // počet nalezených uživatelů:  $res->num_rows;
       // ověření, že dle zadaných údajů je nalezen právě jeden uživatel
@@ -59,6 +74,51 @@ public function verify_player($login,$password)
         return false;
        }
   }
+ 
+public function insert_new_player($email,$nick,$password)
+  {
+    // kontrola, zda už není přihlašovací jméno použité v tabulce potvrzených hráčů
+   $res = $this->conn->query("SELECT `id`,`nick`,`email`  FROM `players` WHERE `nick`='" . $nick . "' OR `email`='" . $email  . "'");
+   echo "<br /> počet nalezených výskytů: " . $res->num_rows . "<br />";
+   if ($res->num_rows > 0)
+     {
+      $res_hrac = $res->fetch_assoc();
+      $res->free_result();
+      if ($email == $res_hrac["email"])
+        {exit ("Hráč s tímto emailem je již registrovaný");}
+      if ($nick == $res_hrac["nick"])
+        {exit ("Hráč s tímto loginem je již registrovaný");}
+     exit ("Hráč je již registrovaný");
+     }
+
+    // kontrola, zda už není přihlašovací jméno použité v tabulce NOVÝCH hráčů
+   $res = $this->conn->query("SELECT `id`,`nick`,`email`  FROM `players_new` WHERE `nick`='" . $nick . "' OR `email`='" . $email  . "'");
+   echo "<br /> počet nalezených výskytů: " . $res->num_rows . "<br />";
+   if ($res->num_rows > 0)
+     {
+      $res_hrac = $res->fetch_assoc();
+      $res->free_result();
+      if ($email == $res_hrac["email"])
+        {exit ("Hráč s tímto emailem je již registrovaný");}
+      if ($nick == $res_hrac["nick"])
+        {exit ("Hráč s tímto loginem je již registrovaný");}
+     exit ("Hráč je již registrovaný");
+     }
+
+   // insert into database:
+   $sql = "INSERT INTO `players_new` (id, nick, email, password) VALUES (0, '" . $nick . "', '" . $email . "', '" . $password . "')";
+   $res = $this->conn->query($sql);
+     
+   if ($this->conn->connect_errno) 
+    {
+     die("Database connection failed.");
+    }
+
+    $res = $this->conn->query("SELECT `id`,`nick` FROM `players` WHERE `nick`='" . $login . "' OR `email`='" . $login . "' AND `password`='" . $password . "'");
+
+        return $res;
+  }
+  
 } 
 
 ?>
