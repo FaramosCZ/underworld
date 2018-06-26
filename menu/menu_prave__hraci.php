4d241df42cc0fb4f23e2c72545ece6ca388aba24
xjarik <40128057+xjarik@users.noreply.github.com>
2018-06-26 17:25:54 +0200

    add menu for level "postavy"
    left and right menu for characters, and connection between all menus.

--- menu/menu_prave__hraci.php
+++ menu/menu_prave__hraci.php
@@ -1,5 +1,6 @@
 <a href="?"><img src=obrazky/vamp_red_mensi.png style="width:100%" /></a>
 
-<a class="menu_a" href="?page=logout">Odhlásit</a>
-
+Tvoje postava:
+<!-- <a class="menu_a" href="?page=logout">Odhlásit</a> -->
+<a class="menu_a" href="?page=character">Jméno postavy</a>
 
